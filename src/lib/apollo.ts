5cc3f46d12d8ed95abc45f51112cd9bf3d76511a
Thiago Machado <contatocostathiago@gmail.com>
2022-06-25 17:59:30 -0300

    feat: creates Subscribe page and adds more conditions on Lesson component

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o3s9pq0eum01xi5zl1hd21/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   // Armazena em cache as requisições, se caso você requisitar um dado que já foi requisitado e está em cache, ele te entrega os dados sem precisar requisitar
   cache: new InMemoryCache(),
 })
